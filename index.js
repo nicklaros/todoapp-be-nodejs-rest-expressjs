5aa7b68da1b7926c0b44725e660da4916334c8e5
eko <eko@sirclo.com>
2022-12-10 11:24:59 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+// Membuat aplikasi express.
+const app = express();
+
+// Menentukan port yang akan dipakai oleh aplikasi express untuk menerima request.
+const port = 3000;
+
+// Membuat default endpoint supaya mengembalikan pesan: halo temen2!.
+app.get('/', (req, res) => {
+    res.send('halo temen2!');
+});
+
+// Mulai menerima request di port yang kita tentukan diatas, 
+// kemudian menampilkan pesan di console ketika semuanya sudah siap.
+app.listen(port, () => {
+    console.log(`api siap menerima request di port ${port}`);
+});
